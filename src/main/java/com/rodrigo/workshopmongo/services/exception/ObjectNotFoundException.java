891272f4f2c72e21686cb13bb8ef836edaa0c328
Rodrigo Martins <rodrigomartinsgb@hotmail.com>
2021-06-15 23:09:19 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/rodrigo/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,13 @@
+package com.rodrigo.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException (String msg) {
+		super(msg);
+	}
+	
+	
+
+}
